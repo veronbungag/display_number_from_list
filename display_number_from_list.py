5049622633dafb0fa248e05c6023223ef8fec14b
veronbungag <rveron.bungag@gmail.com>
2024-01-24 00:08:53 +0800

    pseudocode

---
+++ display_number_from_list.py
@@ -0,0 +1,7 @@
+#define a function for list
+#for loop through the list using list comprehension and return numbers that divisible by five
+#list of numbers
+#print the given list:
+#getting numbers divisible by 5
+#print divisible by 5:
+#print numbers divisible by 5
