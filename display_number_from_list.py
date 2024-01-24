d9ffc7e5b5ae8fbfd08ff678ec1e058b2b71d586
veronbungag <rveron.bungag@gmail.com>
2024-01-24 09:11:27 +0800

    given list added

--- display_number_from_list.py
+++ display_number_from_list.py
@@ -3,6 +3,7 @@ def divisible_by_five(list):
 #for loop through the list using list comprehension and return numbers that divisible by five
     return [numbers for numbers in list if numbers % 5 == 0]
 #list of numbers
+numbers = [10, 20, 33, 46, 55]
 #print the given list:
 #getting numbers divisible by 5
 #print divisible by 5:
